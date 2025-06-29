bf549e1c0782e9b9bfdba27df5b972c2a405452c
Leopoldo Brines <leopoldo.brines@gmail.com>
2025-06-29 16:33:51 -0300

    [main] Fix yarn installation curl syntax error

--- scripts/setup/install_dependencies.sh
+++ scripts/setup/install_dependencies.sh
@@ -191,7 +191,7 @@ main() {
         
         # Install Yarn
         if ! command_exists yarn; then
-            curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - >> "$LOG_FILE" 2>&1
+            apt-key add - >> "$LOG_FILE" 2>&1 < <(curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg)
             echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
             apt-get update >> "$LOG_FILE" 2>&1
             install_package yarn
